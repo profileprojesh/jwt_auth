151355667828621f4e4711a06db9f6c44b7b6f63
projesh <projeshbasnet@gmail.com>
2021-09-13 05:50:53 +0545

    made some changes on __init__.py file of project directory

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
+from flask.templating import Environment
+# import sqlalchemy
+
+
 class Config(object):
     DEBUG=False
     TESTING=False
     SECRET_KEY = "my_top_secret_key"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 class DevelopmentConfig(Config):
+    Environment='development'
     DEBUG=True
     SESSION_COOKIE_SECURE = False
     TESTING=True
--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,3 @@
-from logging import DEBUG
 import bcrypt
 from flask import Flask, app
 from flask_sqlalchemy import SQLAlchemy
@@ -7,46 +6,34 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_manager
 db = SQLAlchemy()
 bcrypt = Bcrypt()
-
 app = Flask(__name__)
 app.config.from_object("config.DevelopmentConfig")
+
+
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    app.config.from_object("config.DevelopmentConfig")
+    db.init_app(app)
+    return app
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-
-
-
-
-"""
-This function is used to just create database tables and after that it should be commented out and uncomment the code below this function
-"""
-# def create_app():
-#     db.init_app(app)
-#     login_manager = LoginManager()
-#     login_manager.login_view = 'views.login_view'
-#     login_manager.init_app(app)
-#     bcrypt.init_app(app)
-
-#     from project.views import auth_blueprint
-#     app.register_blueprint(auth_blueprint)
-#     return app
-
-
-
-
 db.init_app(app)
 login_manager = LoginManager()
 login_manager.login_view = 'views.login_view'
 login_manager.init_app(app)
 bcrypt.init_app(app)
 
-login_manager = LoginManager()
-login_manager.login_view = 'views.login_view'
-login_manager.init_app(app)
-
 from .models import User
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-
+    
 from project.views import auth_blueprint
 app.register_blueprint(auth_blueprint)
+
+
+
